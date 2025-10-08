56b8707d01c9050f3850be07e47435ea6aa93cbe
Trapti10 <traptivishwakarma2007@gmail.com>
2025-10-08 21:17:30 +0530

    To-Do List

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+function addTask(){
+    if(inputBox.value.trim() === ''){ 
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let editBtn = document.createElement("img")
+        editBtn.src = "images/Edit-icon.png";
+        editBtn.classList.add("editBtn")
+        li.appendChild(editBtn)
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";   // To clear the input feild after adding the text  
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+        
+    }
+    else if(e.target.tagName === "SPAN"){
+        
+        e.target.parentElement.remove();   // parent element is li , so it will remove it(li)
+        saveData();
+    }
+    const editbtn = e.target.querySelector(".editBtn")
+    if(e.target.classList.contains("checked")){
+        editbtn.style.pointerEvent = "none";
+        editbtn.style.opacity ="0.5";
+        editbtn.classList.add("disabled"); 
+        saveData();
+        
+    }
+    else{
+        editbtn.style.pointerEvent = "auto";
+        editbtn.style.opacity ="1";
+        editbtn.classList.remove("disabled");
+        saveData();
+        
+    }
+  
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+inputBox.addEventListener("keydown", (e)=>{
+   if(e.key === "Enter"){
+    addTask();
+   }
+})
